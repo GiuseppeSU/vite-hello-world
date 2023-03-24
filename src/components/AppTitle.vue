b60aebfab79427ce0470d624ba64ebe37cbd876a
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-24 15:45:35 +0100

    Creazione titolo con vite

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
