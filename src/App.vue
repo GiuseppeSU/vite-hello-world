ee6cc412623e7e93c70052cfa59ef27c33f3ca3b
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-24 16:27:04 +0100

    Implementazione bonus + fine esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue';
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/hero.vue';
+
 </script>
 
 <template>
   <AppTitle msg="La mia prima app con Vite!" />
+  <Hero />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/hero.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="jumbotron bg-primary text-light w-50 m-auto mt-5 text-center">
+        <h1 class="display-4 ">Hello, world!</h1>
+        <p class="lead ms_color">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+            to
+            featured content or information.</p>
+        <hr class="my-4">
+        <p>è una figata pazzesca</p>
+        <p class="lead">
+            <a class="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+</template>
+
+<style scoped></style>
